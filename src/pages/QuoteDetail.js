7a153270aac4e434b3ac058b4e3e7f9d3ae71d3f
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-02 09:22:54 -0500

    fullscreen quote added to quote detail page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,22 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+import { DUMMY_QUOTES } from "./quotes";
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteID);
+
+  if (!quote) {
+    return <p>No quotes found with that ID.</p>;
+  }
+
   return (
     <Fragment>
-      <div>QuoteDetail Page</div>
-      <p>{params.quoteID}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteID}/comments`}>
         <Comments />
       </Route>
