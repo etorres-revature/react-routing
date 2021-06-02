78734f606ae59176c36c133b91b52c5819a1eaf7
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-02 10:05:17 -0500

    updating nested paths with match form react router dom

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,11 +1,12 @@
 import { Fragment } from "react";
-import { Route, useParams, Link } from "react-router-dom";
+import { Route, useParams, Link, useRouteMatch } from "react-router-dom";
 import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 import { DUMMY_QUOTES } from "./quotes";
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
 
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteID);
@@ -17,14 +18,14 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route exact path={`/quotes/${params.quoteID}`}>
+      <Route exact path={match.path}>
         <div className="centered">
-          <Link className="btn--flat" to={`/quotes/${params.quoteID}/comments`}>
+          <Link className="btn--flat" to={`${match.url}/comments`}>
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteID}/comments`}>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
