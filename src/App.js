e5e3c53fcd0415f6aa3b6acf9e437db7ffa8b733
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-02 08:35:15 -0500

    new project files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { Switch, Route, Redirect } from "react-router-dom";
-import Header from "./component/Header";
-import ProductDetail from "./pages/ProductDetail";
-import Products from "./pages/Products";
-import Welcome from "./pages/Welcome";
-
 function App() {
   return (
     <div>
-      <Header />
-      <main>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/welcome" />
-          </Route>
-          <Route path="/welcome">
-            <Welcome />
-          </Route>
-          <Route exact path="/products">
-            <Products />
-          </Route>
-          <Route path="/products/:productID">
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
+
     </div>
   );
 }
 
 export default App;
-
-//the-domain.com/welcome => Welcome Component
-//the-domain.com/prdoucts => Product Component
-//the-domain.com/product-detail/<any value> => Product Detail Component
