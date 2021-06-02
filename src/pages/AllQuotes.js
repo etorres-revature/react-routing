b97b3b5ea46a4848ce27ea952c5a813e6db17b21
Eric D. Torres <eric.d.torres@ibm.com>
2021-06-02 09:07:16 -0500

    quotes added and allquotes updated

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,8 @@
+import QuoteList from "../components/quotes/QuoteList";
+import { DUMMY_QUOTES } from "./quotes";
+
 const AllQuotes = () => {
-  return <div>AllQuote Page</div>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
---
+++ src/pages/quotes.js
@@ -0,0 +1,10 @@
+export const DUMMY_QUOTES = [
+  { id: "q1", author: "Yoda", text: "Do or do not; there is no try" },
+  {
+    id: "q2",
+    author: "Kevin Torres",
+    text: "If you didn't  have time to do it right the first time, how will you ever have time to do it right the second?",
+  },
+  { id: "q3", author: "Bradley", text: "Man who stand on toilet, high on pot" },
+  { id: "q4", author: "Max", text: "Learning React is great!!" },
+];
